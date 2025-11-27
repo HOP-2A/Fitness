7465e132ec7f0bebbe84929042e0f13e51115939
25HP5149@nest.edu.mn <25HP5149nest.edu.mn@pinecones-iMac.local>
2025-11-27 18:00:55 +0800

    ads

--- app/api/getExercise/route.ts
+++ app/api/getExercise/route.ts
@@ -2,7 +2,6 @@ import { NextResponse } from "next/server";
 import { prisma } from "@/app/lib/route";
 
 export async function GET() {
-  console.log("qwoewoiejfpoisjdfpsdfjkospodjfpiosd");
   const exercises = await prisma.assignedExercise.findMany();
 
   return NextResponse.json({ message: exercises, status: 200 });
