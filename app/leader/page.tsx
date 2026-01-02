456e01955fbfd8eef755fd3ec4ee84ea9843a964
25HP5149@nest.edu.mn <25HP5149nest.edu.mn@pinecones-iMac.local>
2026-01-02 16:52:28 +0800

    1

--- app/leader/page.tsx
+++ app/leader/page.tsx
@@ -1,9 +1,12 @@
+import ShowCarousel from "../_components/Carousel";
 import { Footer } from "../_components/Footer";
 import { Leaderboard } from "../_components/Leaderboard";
 
 const Leader = () => {
   return (
     <div>
+      <ShowCarousel />
+
       <div>
         <Leaderboard />
       </div>
